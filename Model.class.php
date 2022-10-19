3eb6a5db344682f4d66438a09a8dddef9c90badb
Alain-Kay <kazadikayumba@gmail.com>
2022-10-19 22:26:31 +0200

    database connection

---
+++ Model.class.php
@@ -0,0 +1,20 @@
+<?php
+
+abstract class Model{
+    private static $pdo;
+
+    private static function setBdd(){
+        self::$pdo = new PDO("mysql:host=localhost;dbname=library;charset=utf8", "root", "alain22021998");
+        self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+    protected function getBdd(){
+        if(self::$pdo === null){
+            self::setBdd();
+        }
+        return self::$pdo;
+    }
+
+   
+    
+}
